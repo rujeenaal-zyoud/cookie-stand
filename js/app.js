5a3835572d00ed49ab782a307f3a67987d504092
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-04-06 07:43:23 +0300

    lab7

--- js/app.js
+++ js/app.js
@@ -1,20 +1,55 @@
 
-const hours=['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let hours=['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 //create the function for random custemer number
 function randomnumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+object=[];
 //create the constructor for all object store cookies
+
+
+let parent= document.getElementById('parent');
+//create table in globel
+let table=document.createElement('table');
+//appeand
+parent.appendChild(table);
+//craet tr element in table
+let headingrow=document.createElement('tr');
+//append tr to table
+table.appendChild(headingrow);
+
+let thelemn=document.createElement('th');
+headingrow.appendChild(thelemn);
+
+for (let i =0;i<hours.length;i++){
+    // create the first row
+     thelemn=document.createElement('th');
+    //appeanding
+    headingrow.appendChild(thelemn);
+    //give the element the contant
+    thelemn.textContent=hours[i];
+
+}
+
+let th2Element = document.createElement('th');
+headingrow.appendChild(th2Element);
+th2Element.textContent = "Daily Location Total";
+
+
 function cookies (name,min,max,avg)
 {
     this.min=min;
     this.max=max;
     this.avg=avg;
 this.name=name;
-this.cookiesperhour=[];
+
 this.total=0;
-this.randomnumber();
+randomnumber();
+ this.cookiesperhour=[];
+
+object.push(this);
+
 }
 
 const Seattle=new cookies('Seattle',23,65,6.3);
@@ -44,30 +79,34 @@ for(let i=0;i<hours.length;i++){
 }
 
 }
+cookies.prototype.render=function (){
+    let storerow=document.createElement('tr');
+    table.appendChild(storerow); 
+    let datarow=document.createElement('td');
+    table.appendChild(storerow);
+
+    for(let i =0;i<hours.length;i++){
+       
+       
+let datarow=document.createElement('td');
+storerow.appendChild(datarow);
+datarow.textContent=`${this.cookiesperhour[i]}`;
+
+        
+    }
+    datarow=document.createElement('td');
+    storerow.appendChild(datarow);
+    datarow.textContent=`${this.total}`;
+
+}
+
+
+
+
+
 
 //create render function dom
 
-let parent= document.getElementById('parent');
-//create table in globel
-let table=document.createElement('table');
-//appeand
-parent.appendChild(table);
-//craet tr element in table
-let headingrow=document.createElement('tr');
-//append tr to table
-table.appendChild(headingrow);
-
-for (let i =0;i<hours.length;i++){
-    // create the first row
-    let thelement=document.createElement('th');
-    //appeanding
-    headingrow.appendChild(thelement);
-    //give the element the contant
-    thelement.textContent=hours[i];
-
-
-
-}
 
 //Tokyo.calcookiesperhour();
 //Seattle.calcookiesperhour();
